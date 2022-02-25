b1cbc909a11b7abd4b2906e49994ccd0564138ca
Vitor Freitas <vitor_freitas_b@hotmail.com>
2022-02-25 19:46:01 -0300

    feat(main): add the main basic file and an utils mod

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use bevy::prelude::*;
+
+#[cfg(all(feature = "debug"))]
+use bevy_inspector_egui::WorldInspectorPlugin;
+
+mod utils;
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum GameState {
+  Playing,
+}
+
 fn main() {
-    println!("Hello, world!");
+  let mut app = App::new();
+
+  app
+    .insert_resource(WindowDescriptor {
+      title: "Template".to_string(),
+      width: 640.0,
+      height: 480.0,
+      vsync: true,
+      ..Default::default()
+    })
+    .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+    .add_plugins(DefaultPlugins)
+    .add_state(GameState::Playing)
+    .add_system(bevy::input::system::exit_on_esc_system);
+
+  #[cfg(all(feature = "debug"))]
+  app.add_plugin(WorldInspectorPlugin::new());
+
+  app.run();
 }
---
+++ src/utils/vec2.rs
@@ -0,0 +1,35 @@
+use bevy::math::*;
+
+const CMP_EPSILON: f32 = 0.000001;
+
+#[allow(dead_code)]
+pub fn direction_to(current: Vec2, to: Vec2) -> Vec2 {
+  return Vec2::new(current.x - to.x, current.y - to.y).normalize();
+}
+
+#[allow(dead_code)]
+pub fn move_toward(current: Vec2, to: Vec2, delta: f32) -> Vec2 {
+  let vd = to - current;
+  let len = vd.length();
+  if len <= delta || len < CMP_EPSILON {
+    return to;
+  }
+
+  return current + vd / len * delta;
+}
+
+#[allow(dead_code)]
+pub fn reflect(velocity: Vec2, normal: Vec2) -> Vec2 {
+  let mut n = Vec2::new(normal.x, normal.y);
+  if !normal.is_normalized() {
+    n = normal.normalize();
+  }
+
+  let dot = velocity.dot(n);
+  return 2. * n * dot - velocity;
+}
+
+#[allow(dead_code)]
+pub fn bounce(velocity: Vec2, normal: Vec2) -> Vec2 {
+  return -(reflect(velocity, normal));
+}
